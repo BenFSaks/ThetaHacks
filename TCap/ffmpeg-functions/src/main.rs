8bdced87ba580a30ed9a7c89435668189766c7fb
Peter Navarrete <navarrep1@newpaltz.edu>
2023-04-23 19:20:26 -0400

    INIT: overlay recording; NOT FUNCTIONAL

--- TCap/ffmpeg-functions/src/main.rs
+++ TCap/ffmpeg-functions/src/main.rs
@@ -1,16 +1,53 @@
-use std::process::Command;
 use std::time::Duration;
+use std::io::{Write, self};
+use std::process::{Command, Stdio};
 fn main() {
-    let mut record = Command::new("ffmpeg");
-    record.arg("-f").arg("gdigrab").arg("-i").arg("desktop").arg("out.wmv");
-    let mut s = record.spawn();
-    let sleep = Duration::from_secs(7);
-    std::thread::sleep(sleep);
-    s.expect("He").kill().expect("cant kill it");
+    println!("video=\"FaceTime HD Camera (Built-in)\":audio=\"Internal Digital Microphone (Apple Audio Device)\"");
+    record_camera_screen(4).expect("didnt work");
 }
 
-fn record_camera_screen() {
-    println!("recording");
+fn record_camera_screen(time: u64) -> io::Result<()> {
+    let vid = "video=\"FaceTime HD Camera (Built-in)\":audio=\"Internal Digital Microphone (Apple Audio Device)\"";
+    //-f gdigrab -thread_queue_size 64 -framerate 30 -i desktop -f dshow -i video="FaceTime HD Camera (Built-in)":audio="Internal Digital Microphone (Apple Audio Device)" -filter_complex 'overlay' overlayed.mp4
+    let mut record = Command::new("ffmpeg")
+
+        .arg("-f")
+        .arg("gdigrab")
+        .arg("-thread_queue_size")
+        .arg("64")
+        .arg("-framerate")
+        .arg("30")
+        .arg("-i")
+        .arg("desktop")
+        .arg("-f")
+        .arg("dshow")
+        .arg("-i")
+        //.arg("video=\"FaceTime")
+        // .arg("HD")
+        // .arg("Camera")
+        // .arg("(Built-in)\":audio=\"Internal")
+        // .arg("Digital")
+        // .arg("Microphone")
+        // .arg("(Apple")
+        // .arg("Audio")
+        // .arg("Device)\"")
+
+        .arg(vid)
+        .arg("-filter_complex")
+        .arg("'overlay'")
+        .arg("overlayed.mp4")
+
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    std::thread::sleep(Duration::from_secs(time+1));
+    let record_stdin = record.stdin.as_mut().unwrap();
+    record_stdin.write_all(b"q")?;
+    drop(record_stdin);
+    let output = record.wait_with_output()?;
+    println!("output = {:?}", output);
+    Ok(())
 }
 //ffmpeg -f dshow -i video="FaceTime HD Camera (Built-in)":audio="Internal Digital Microphone (Apple Audio Device)" out.mp4
 //above command records my camera and audio nicely but it has a 4 second delay from the start 
