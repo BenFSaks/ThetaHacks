83f3ea58e98d034faafaa01f3df73bd0ac21fb6a
Charles Olinsky <charlesolinsky@gmail.com>
2023-04-25 17:53:33 -0400

    ADD: Generate Window + Get Window Size
    -A problem still exists where the source code for any created window is shown instead of the compiled typescript

--- TCap/src-tauri/src/main.rs
+++ TCap/src-tauri/src/main.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 //Imports
-use tauri::{window::WindowBuilder, AppHandle, WindowUrl};
+//use tauri::{window::WindowBuilder, AppHandle, WindowUrl};
 
 //TUARI COMMANDS 
 #[tauri::command]
@@ -13,31 +13,28 @@ println!("REC: In Progress");
 fn stop_recording() {
 println!("REC: Stopped");
 }
-#[tauri::command]
-async fn open_settings_window(app: AppHandle) -> Result<(), String> {
-    let result = WindowBuilder::new(&app, "settings", WindowUrl::App("/src/Settings.tsx".into()))
-        .fullscreen(false)
-        .resizable(true)
-        .title("Settings")
-        .center()
-        //.inner_size(800.0, 600.0)
-        .build();
-    match result {
-        Ok(_) => {
-            println!("Settings Window Created Successfully!");
-            Ok(())
-        }
-        Err(err) => {
-            println!("Failed to Create Settings Window {}", err);
-            Err("Failed to create Window".to_string())
-        }
-    }
-}
-
-
+// #[tauri::command]
+// async fn open_settings_window(app: AppHandle) -> Result<(), String> {
+//     let result = WindowBuilder::new(&app, "settings", WindowUrl::App("/src/Settings.jsx".into()))
+//         .fullscreen(false)
+//         .resizable(true)
+//         .title("Settings")
+//         .center()
+//         .build();
+//     match result {
+//         Ok(_) => {
+//             println!("Settings Window Created Successfully!");
+//             Ok(())
+//         }
+//         Err(err) => {
+//             println!("Failed to Create Settings Window {}", err);
+//             Err("Failed to create Window".to_string())
+//         }
+//     }
+// }
 
 fn main() {
-tauri::Builder::default()
+    tauri::Builder::default()
     //Window Config via setup Hook
     // .setup(|app| {
     //     let new_window = tauri::WindowBuilder::new(
@@ -50,7 +47,7 @@ tauri::Builder::default()
 
     //Invoke Functions 
     .invoke_handler(tauri::generate_handler![start_recording])
-    .invoke_handler(tauri::generate_handler![open_settings_window])
+    //.invoke_handler(tauri::generate_handler![open_settings_window])
     
     //Tauri App Generator 
     .run(tauri::generate_context!())
--- TCap/src/App.tsx
+++ TCap/src/App.tsx
@@ -3,24 +3,45 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 //TAURI
 import { invoke } from "@tauri-apps/api/tauri";
+import { LogicalPosition, WebviewWindow } from '@tauri-apps/api/window'
+import { appWindow } from "@tauri-apps/api/window";
 //STYLES
 import "./styles/App.css";
+import { error } from "console";
 
 
 function App() {
 
-  return (
-    <div id='menu-container'>
-            <div id='menu-header'>
-            <h1>TCap</h1>
-            </div>
+  async function open_settings_window(){
 
-            <div id='menu-buttons'>
-                <button type='button'>RECORD</button>
-                <button type='button' onClick={() => invoke('open_settings_window')}>SETTINGS</button>
-            </div>
-        </div>
-  );
-}
+    //PROBLEM: can only view source 
+    const webview = new WebviewWindow('Settings', {
+      url: 'src/Settings.tsx',
+    })
+
+    // wait for the webview window to be created
+    await new Promise<void>((resolve, reject) => {
+      webview.once('tauri://created', () => resolve())
+      webview.once('tauri://error', () => reject())
+    })
+
+    // get the physical size of the webview window
+    const physicalSize = await webview.innerSize()
+    console.log(`Settings Window Opened Successfully, Physical Size: ${physicalSize.width}x${physicalSize.height}`)
+  }
+
+    return (
+      <div id='menu-container'>
+              <div id='menu-header'>
+              <h1>TCap</h1>
+              </div>
+
+              <div id='menu-buttons'>
+                  <button type='button'>RECORD</button>
+                  <button type='button' onClick={() => open_settings_window()}>SETTINGS</button>
+              </div>
+          </div>
+    );
+  }
 
 export default App;
