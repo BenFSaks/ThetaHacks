a37035128a0598259a238aae5fe543f8812dc6db
Peter Navarrete <navarrep1@newpaltz.edu>
2023-04-26 17:02:23 -0400

    FEAT: recording overlayed camera and screen video. get dshow devices

--- TCap/ffmpeg-functions/src/main.rs
+++ TCap/ffmpeg-functions/src/main.rs
@@ -2,11 +2,14 @@ use std::time::Duration;
 use std::io::{Write, self};
 use std::process::{Command, Stdio};
 fn main() {
-    // //-f gdigrab -thread_queue_size 64 -framerate 30 -i desktop -f dshow -i video="FaceTime HD Camera (Built-in)":audio="Internal Digital Microphone (Apple Audio Device)" -filter_complex 'overlay' overlayed.mp4
-    get_dshow_devices();
+
 }
 
-fn get_dshow_devices() {
+
+// this function will return a vector of strings that are the names of the dshow devices
+fn get_dshow_devices() -> Vec<String> {
+
+
     let mut list = Command::new("ffmpeg")
         .arg("-list_devices")
         .arg("true")
@@ -14,24 +17,49 @@ fn get_dshow_devices() {
         .arg("dshow")
         .arg("-i")
         .arg("dummy")
-        // Tell the OS to record the command's output
         .stdout(Stdio::piped())
-        // execute the command, wait for it to complete, then capture the output
         .output()
-        .expect("failed to execute process");
+        .unwrap();
+        // this command lists all the dshow devices and captures its output
 
-    // extract the raw bytes that we captured and interpret them as a string
-    //let output = list.wait_with_output()?;
-    let output = list.wait_with_output().expect("failed to wait on child");
-    let stdout = String::from_utf8().unwrap();
-    println!("stdout = {:?}", stdout);
+    // convert output of command into a string
+    let stdout = String::from_utf8(list.stderr).unwrap();
+
+    // parse the string into strings surrounded by quotes
+    let mut words = stdout.split("\"");
+
+    // create a vector of strings that will hold the names of the devices
+    let mut devices:Vec<String> = Vec::new();
+    for word in words.clone() {
+        if word.contains(" (video)")
+            || word.contains(" (audio")
+            || word.contains("dshow")
+            || word.contains("dummy")
+            || word.contains("@device")
+        {
+            continue;
+        }
+        devices.push(word.to_string());
+    }
+
+    // return the devices;
+    devices
 }
 
 
-fn record_camera_screen(time: u64) -> io::Result<()> {
-    let vid = "video=\"FaceTime HD Camera (Built-in)\" ";
-    let vid2 = "video=\"@device_pnp_\\?\\usb#vid_05ac&pid_8514&mi_00#7&14e17b62&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global\"";
-    // //-f gdigrab -thread_queue_size 64 -framerate 30 -i desktop -f dshow -i video="FaceTime HD Camera (Built-in)":audio="Internal Digital Microphone (Apple Audio Device)" -filter_complex 'overlay' overlayed.mp4
+
+
+fn record_camera_screen(time: u64, video: &String, audio: &String) -> io::Result<()> {
+
+    // we format the video and audio strings into the format that ffmpeg wants
+    let mut vid = "video=".to_owned();
+    vid.push_str(video);
+    vid.push_str(":");
+    vid.push_str("audio=");
+    vid.push_str(audio);
+
+    // this is the command that will be executed
+    // to record screen, camera and mic
     let mut record = Command::new("ffmpeg")
 
         .arg("-f")
@@ -45,7 +73,7 @@ fn record_camera_screen(time: u64) -> io::Result<()> {
         .arg("-f")
         .arg("dshow")
         .arg("-i")
-        .arg("video=FaceTime HD Camera (Built-in):audio=Internal Digital Microphone (Apple Audio Device)")
+        .arg(vid)
         .arg("-filter_complex")
         .arg("'overlay'")
         .arg("overlayed.mp4")
@@ -54,19 +82,14 @@ fn record_camera_screen(time: u64) -> io::Result<()> {
         .stdout(Stdio::piped())
         .spawn()?;
 
+
+    //record for time amount of seconds
+    //+1 is because it stops the recording a second early for some reason
     std::thread::sleep(Duration::from_secs(time+1));
+
+    // send a q to the stdin of the ffmpeg process to stop recording
     let record_stdin = record.stdin.as_mut().unwrap();
     record_stdin.write_all(b"q")?;
-    drop(record_stdin);
-    let output = record.wait_with_output()?;
-    println!("output = {:?}", output);
+
     Ok(())
  }
-//ffmpeg -f dshow -i video="FaceTime HD Camera (Built-in)":audio="Internal Digital Microphone (Apple Audio Device)" out.mp4
-//above command records my camera and audio nicely but it has a 4 second delay from the start 
-
-// ffmepg -i basefile.wmv -vf "movie=secondfile.wmv scale=300:-1 [inner];[in][inner] overlay=10:10 [out]" overlay.mp4
-//                                            scale changes size of video
-//-f gdigrab -thread_queue_size 64 -framerate 30 -i desktop -f dshow -i video="FaceTime HD Camera (Built-in)":audio="Internal Digital Microphone (Apple Audio Device)" -filter_complex 'overlay' overlayed.mp4
-// the above command captures screen and camera and audio and overlays the camera on the top left.
-// I have no idea how to change a few things like where it is and how large the video is
