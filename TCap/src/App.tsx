bd0f63dec824e47cc22501a39d9f825eb8f239b1
Charles Olinsky <charlesolinsky@gmail.com>
2023-05-05 11:27:02 -0400

    EDIT: Transparent Progress

--- TCap/src/App.tsx
+++ TCap/src/App.tsx
@@ -10,17 +10,13 @@ import { appWindow } from "@tauri-apps/api/window";
 import "./styles/App.css";
 
 
-
 function App() {
 
   async function open_settings_window(){
 
-<<<<<<< Updated upstream
-    //PROBLEM: can only view source 
-=======
->>>>>>> Stashed changes
     const webview = new WebviewWindow('Settings', {
       url: '../settings.html',
+      title: "Settings"
     })
 
     // wait for the webview window to be created
@@ -33,15 +29,21 @@ function App() {
     const physicalSize = await webview.innerSize()
     console.log(`Settings Window Opened Successfully, Physical Size: ${physicalSize.width}x${physicalSize.height}`)
   }
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
+  function open_crop_window(){
+
+    const webview = new WebviewWindow('Crop', {
+      url: '../crop.html',
+      decorations: true, 
+      transparent: true, 
+    })
+  }
 
     return (
       <div id='menu-container'>
               <div id='menu-header'>
-              <h1>TCap</h1>
+                <h1>TCap</h1>
+                  <input type="text" id="path"/>
               </div>
 
               <div id='preview-panel'>
@@ -51,10 +53,7 @@ function App() {
               <div id='panel-container'>
                 <div id='record-panel'>
                     <h3>RECORD</h3>
-<<<<<<< Updated upstream
-=======
-                    <button type='button'>CROP</button>
->>>>>>> Stashed changes
+                    <button type='button' onClick={() => open_crop_window()}>CROP</button>
                     <button type='button'>START</button>
                     <button type='button'>PAUSE</button>
                     <button type='button'>STOP</button>
