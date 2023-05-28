bb19f15e1fe821de8fba47217923b4eff6883c2d
Peter Navarrete <navarrep1@newpaltz.edu>
2023-05-28 12:30:00 -0400

    STYLE: centered divs, made a little cleaner

--- TCap/src/App.tsx
+++ TCap/src/App.tsx
@@ -7,84 +7,62 @@ import Config from "./Config";
 
 function App() {
 
-  //States 
-  const [audioInput, setAudioInput] = useState<string[]>([]); 
-  const [videoInput, setVideoInput] = useState<string[]>([]); 
+    //States 
+    const [audioInput, setAudioInput] = useState<string[]>([]); 
+    const [videoInput, setVideoInput] = useState<string[]>([]); 
 
-  //Open a new window called settings 
-  async function open_settings_window(){
+    //Open a new window called settings 
+    async function open_settings_window(){
 
-    const webview = new WebviewWindow('Settings', {
-      url: '../settings.html',
-      title: "Settings"
-    })
+        const webview = new WebviewWindow('Settings', {
+url: '../settings.html',
+title: "Settings"
+})
 
-    // wait for the webview window to be created
-    await new Promise<void>((resolve, reject) => {
-      webview.once('tauri://created', () => resolve())
-      webview.once('tauri://error', () => reject())
-    })
+// wait for the webview window to be created
+await new Promise<void>((resolve, reject) => {
+        webview.once('tauri://created', () => resolve())
+        webview.once('tauri://error', () => reject())
+        })
 
-    // get the physical size of the webview window
+// get the physical size of the webview window
     const physicalSize = await webview.innerSize()
-    console.log(`Settings Window Opened Successfully, Physical Size: ${physicalSize.width}x${physicalSize.height}`)
-  }
+console.log(`Settings Window Opened Successfully, Physical Size: ${physicalSize.width}x${physicalSize.height}`)
+    }
 
-  //Open transparent cropable window 
-  function open_crop_window(){
+//Open transparent cropable window 
+function open_crop_window(){
 
     const webview = new WebviewWindow('Crop', {
-      url: '../crop.html',
-      decorations: true, 
-      transparent: true, 
-    })
-  }
+url: '../crop.html',
+decorations: true, 
+transparent: true, 
+})
+}
 
-  //Method to get input options (audio, video, cam)
-  const fetchInputData = async() => { 
-      const audio: Array<string> = await invoke('get_dshow_devices');
-      const video: Array<string> = await invoke('get_dshow_devices');
-      
-      setAudioInput(audio);
-      setVideoInput(video);  
-  }
-  useEffect(() => {
-    fetchInputData();
-  }, [])
+//Method to get input options (audio, video, cam)
+const fetchInputData = async() => { 
+    const audio: Array<string> = await invoke('get_dshow_devices');
+    const video: Array<string> = await invoke('get_dshow_devices');
 
-    return (
-      <div id='menu-container'>
-              {/* <div id='menu-header'>
-                <h1>TCap</h1>
-              </div>
+    setAudioInput(audio);
+    setVideoInput(video);  
+}
+useEffect(() => {
+        fetchInputData();
+        }, [])
 
-              <div id='preview-panel'>
-                <h3>PREVIEW</h3>
-              </div>
-
-              <div id='panel-container'>
-                <div id='record-panel'>
-                    <h3>RECORD</h3>
-                    <button type='button' onClick={() => open_crop_window()}>CROP</button>
-                    <button type='button'>START</button>
-                    <button type='button'>PAUSE</button>
-                    <button type='button'>STOP</button>
-                    
-                </div>
-                <div id='config-panel'>
-                    <h3>CONFIG</h3>
-                    <button type='button' onClick={() => open_settings_window()}>SETTINGS</button>
-                    <button type='button'>EDIT</button>
-                    <button type='button'>SHARE</button>
-                </div>
-              </div> */}
-
-          <Config 
-            videoValues = {videoInput}
-            audioValues = {audioInput}
-          />
-      </div>
-    );
-  }
+return (
+        <>
+            <h1 className="title">
+                TCap Screen Recorder
+            </h1>
+         <Config 
+         videoValues = {videoInput}
+         audioValues = {audioInput}
+         />
+        </>
+       );
+}
 
 export default App;
--- TCap/src/Config.tsx
+++ TCap/src/Config.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-
+        
 const Config = (props: { audioValues: Array<string>, videoValues: Array<string>}) => {
     
     const [isRecording, setRecording] = useState(false);
@@ -34,41 +34,41 @@ const Config = (props: { audioValues: Array<string>, videoValues: Array<string>}
 
 
     return(
-        <div id='config'>
-            
+    <div className='parent'>
+        <div className='config'>
             {/* VIDEO CONFIG SETTINGS */}
-            <div id='config-record'>
-                <h1>Record</h1>
+            <div id='config-record' className='config-option'>
+                <h1>Video</h1>
                 <Dropdown 
                     options={props.videoValues} onChange={e => setVideoInput(e.value)} placeholder="Select Input"/>
             </div>
-            
+                     
 
             {/* AUDIO CONFIG SETTINGS */}
-            <div id='config-audio'>
+            <div id='config-audio' className='config-option'>
                 <h1>Audio</h1>
                 <Dropdown options={props.audioValues} onChange={e => setAudioInput(e.value)} placeholder="Select Input"/>
             </div>
 
-
+            
             {/* PATH CONFIG SETTINGS */}
-            <div id='config-path'>
+            <div id='config-path' className='config-path'>
                 <h1>Path</h1>
-                <label htmlFor='path'>Record Path </label>
+                <label htmlFor='path'></label>
                 <input type="text" onChange={e => setPath(e.target.value)} placeholder="/" name="path"/>
             </div>
 
 
             {/* DURATION CONFIG SETTINGS */}
-            <div id='config-duration'>
+            <div id='config-duration' className='config-duration'>
                 <h1>Duration</h1>
-                <label htmlFor='path'>Duration(s) </label>
+                <label htmlFor='path'></label>
                 <input type="number" onChange={e => setDuration(e.target.valueAsNumber)} placeholder="00:00" name="duration" min="1" max="15"/>
             </div>
 
 
             {/* RECORD BUTTON */}
-            <div id='config-action-button'>
+            <div id='config-action-button' className='config-record'>
                 <button type='button' onClick={()=> recordToDesktop(
                     duration,
                     videoInput,
@@ -80,6 +80,7 @@ const Config = (props: { audioValues: Array<string>, videoValues: Array<string>}
                 <h3>REC: {isRecording.toString().toUpperCase()}</h3>
             </div>
         </div>
+    </div>
     )
 }
 export default Config; 
