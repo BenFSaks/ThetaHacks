5761215523d401b64e721c9d60fce6a7f9ae4b31
BenFSaks <83617955+BenFSaks@users.noreply.github.com>
2023-05-31 12:49:43 -0400

    feat: arrow functionality

--- ThetaTok/FE_Theta_Tok/src/Video.tsx
+++ ThetaTok/FE_Theta_Tok/src/Video.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import firebase from "firebase/compat";
 import './Video.css'
+import { User } from '@firebase/auth';
 
 interface Props {
   firebaseApp: firebase.app.App;
@@ -32,24 +33,42 @@ export const Video: React.FC<Props> = ({firebaseApp}) => {
                     })
                 }
             })
-            setVideos(userVideos);
-            console.log(allUsers)
+            setVideos(shuffleList(userVideos));
         } catch (error) {
             console.log(error);
         }
     }
-    const generateRandomNumber = () => {
-        const newRandomNumber = Math.floor(Math.random() * videos.length);
-        setCurrVid(newRandomNumber);
+    const shuffleList = (list: any[]) => {
+        const shuffledList = [...list];
+        for (let i = shuffledList.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledList[i], shuffledList[j]] = [
+                shuffledList[j],
+                shuffledList[i],
+            ];
+        }
+        return shuffledList;
     };
+    const outOfBoundsUp = () =>{
+        if(currVid <= 0){
+            return true;
+        } 
+        return false;
+    }
+    const outOfBoundsDown = () =>{
+        if(currVid >= videos.length-1){
+            return true;
+        } 
+        return false;
+    }
 
 
   return (
     <div className='video'>
         {videos[currVid] ? <iframe src={videos[currVid].video_src} frameborder="0" unMute allowFullScreen width="500" height="760"></iframe> : <></>}
         <div>
-            <button onClick={()=> console.log(currVid)}>↑</button>
-            <button onClick={()=> generateRandomNumber()}>↓</button>
+            <button disabled={outOfBoundsUp()} onClick={()=> setCurrVid(currVid-1)}>↑</button>
+            <button disabled={outOfBoundsDown()} onClick={()=> setCurrVid(currVid+1)}>↓</button>
 
         </div>
     </div>
